ce074892ae6d815645aaf54f6630d7077e617753
Anatolii Kuzhlev <anatolii.kuzhlev@tuni.fi>
2023-12-10 18:52:26 +0200

    completed module6

--- module4/a6/6.js
+++ module4/a6/6.js
@@ -2,10 +2,11 @@
 
 let button = document.getElementById("start");
 
-button.addEventListener("click", async function clicked() {
+button.addEventListener("click", async function(event) {
     try {
+        event.preventDefault();
         let value_from_input = document.getElementById("query").value;
-        console.log(value_from_input);
+
         const proxy = 'https://users.metropolia.fi/~ilkkamtk/proxy.php?url=';
         const search = `https://api.chucknorris.io/jokes/search?query=${value_from_input}`;
         const url = proxy + encodeURIComponent(search);
@@ -14,7 +15,14 @@ button.addEventListener("click", async function clicked() {
         const data = await response.json();
         console.log(data);
 
-        
+        for (let i = 0; i < data.result.length; i++) {
+            let article = document.createElement("article");
+            let joke = document.createElement("p");
+            joke.innerHTML = data.result[i].value;
+            article.appendChild(joke);
+            document.body.appendChild(article);
+        }
+
     } catch (error) {
         console.error("Error:", error.message);
     }
